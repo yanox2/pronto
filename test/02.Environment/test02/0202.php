947ef88521c191767de33fec00146499ac9daf14
yanox2 <yano@dodat.jp>
2017-08-04 00:45:00 +0900

    Add files via upload

--- test/02.Environment/test02/0202.php
+++ test/02.Environment/test02/0202.php
@@ -4,70 +4,44 @@
  * Test
  *      Request情報
  *---------------------------------------------------------------------------*/
-require_once('../../path.inc');
-require_once(C_PR_HOME_PATH.'/test/02.Environment/test01/sysdef_org.inc');
+require_once('../../etc/path.inc');
+require_once('./sysdef_org.inc');
 
 include('../../etc/header.tpl');
 ?>
+<div id="lay_center" class="col span_14" style="min-height:1000px; padding:30px;">
+<header style="margin-bottom:20px; font-size:20px;">●Request情報</header>
+<article style="font-size:14px;">
 
-<header>●Request情報</header>
-<article>
-<section style="font-size:14px;">
-<div style="margin:10px 0;"><?=C_PR_HOME_PATH.'/test/02.Environment/test02/sysdef_org.inc を読み込み'?></div>
-
-<?php
-
-$homeDir = '/'.\PR\HTTPUtil::formatURL(C_PR_HOME_PATH);
-$insDir = '/'.\PR\HTTPUtil::formatURL(C_PR_INSTALL_PATH);
-$testDir = '/'.\PR\HTTPUtil::formatURL(C_PR_TEST_ROOT).'/02.Environment/test02';
-
-$host = $_SERVER['HTTP_HOST'];
+<?
+\PR\SysEnv::setTraceLevel(1);
+$uriRoot = $root.'/02.Environment/test02';
+$st = intval($_GET['st']);
+$host = \PR\Request::$sHOST;
 $pos = strpos($host,'.') + 1;
 $domain = substr($host,$pos);
 
-$uriRoot = $testDir.'/root';
-
-if(intval($_GET['st']) > 0){
-	\PR\SysEnv::setYourDocRoot($testDir);
-	\PR\SysEnv::setTraceLevel(1);
-}
-
-if($_GET['st'] == 2){
-	if(!empty($domain)){
-		\PR\SysEnv::setSubSysMode(1);
-		\PR\SysEnv::setSubSysMainDomain($domain);
+if($st > 0){
+	\PR\SysEnv::setYourDocRoot($uriRoot); // doc_root指定
+	if($st == 2){ // サブシステムの判定（サブドメイン）
+		if(!empty($domain)){
+			\PR\SysEnv::setSubSysMode(1);
+			\PR\SysEnv::setSubSysMainDomain($domain);
+		}
+	}else if($st == 3){ // サブシステムの判定（サブディレクトリ）
+		\PR\SysEnv::setSubSysMode(2);
+		\PR\SysEnv::setSubSysURIRoot($uriRoot.'/root');
 	}
-}
-if($_GET['st'] == 3){
-	\PR\SysEnv::setSubSysMode(2);
-	\PR\SysEnv::setSubSysURIRoot($uriRoot);
 }
 
 // リクエスト情報
 \PR\Request::println();
-
-print '<div style="height:200px; margin:10px 0; padding:10px; font-size:12px; border:1px solid black; overflow-y:scroll; overflow-x:visible;">'."\n";
-print '<div>・URI = doc_rootのディレクトリ構造</div>'."\n";
-$url = $testDir.'/0202.php';
-print '<div><a href="'.$url.'">phpファイルを直接指定</a></div>'."\n";
-$url = $testDir.'/';
-print '<div><a href="'.$url.'">phpファイル名を省略</a></div>'."\n";
-$url = $testDir.'/aaa/bbb';
-print '<div><a href="'.$url.'">phpファイル名を省略</a>（最後尾の"/"なし）</div>'."\n";
-$url = $testDir.'/aaa/bbb/?st=1';
-print '<div><a href="'.$url.'">システムのドキュメントルートを指定</a>（C_PR_YOUR_DOCROOT="'.$testDir.'"）</div>'."\n";
-$url = $testDir.'/aaa/bbb/?st=2';
-print '<div><a href="'.$url.'">サブシステムの判定</a>（C_PR_SUBSYS_MODE="1", C_PR_SUBSYS_MAINDOMAIN="'.$domain.'"）</div>'."\n";
-$url = $testDir.'/root/subsys1/ccc/?st=3';
-print '<div><a href="'.$url.'">サブシステムの判定</a>（C_PR_SUBSYS_MODE="2", C_PR_SUBSYS_URIROOT="'.$uriRoot.'"）</div>'."\n";
-
-print '</div>'."\n";
-
 ?>
 
-</section>
 </article>
+</div>
 
 <?php
+include('./right.tpl');
 include('../../etc/footer.tpl');
 ?>
